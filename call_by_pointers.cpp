561b1565d649b3c71874b450493801b797e61796
Harshit <harshit0580.be21@chitkara.edu.in>
2022-04-11 22:03:27 +0530

    Calling differences

---
+++ call_by_pointers.cpp
@@ -0,0 +1,20 @@
+// example of call by reference
+
+#include <iostream>
+using namespace std;
+
+void swap(int *x, int *y)
+{
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+int main()
+{
+    int x=20;
+    int y=26;
+    swap(&x, &y);
+    cout << "Values after swap:  x=" << x << "   y=" << y;
+    return 0;
+}
